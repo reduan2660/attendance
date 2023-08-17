cf9ca784e1826978500b73626fa04e0448da8317
Alve Reduan <reddddddddd@Alves-MacBook-Air.local>
2023-08-17 16:12:01 +0600

    live url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,5 @@
-export const BACKEND_URL = 'http://localhost:8000';
-export const WSS_URL = 'ws://localhost:8000';
+export const BACKEND_URL = 'http://attendance.eis.du.ac.bd/api';
+// export const BACKEND_URL = 'http://localhost:8000';
+
+// export const WSS_URL = 'ws://localhost:8000';
+export const WSS_URL = 'ws://attendance.eis.du.ac.bd/api';
