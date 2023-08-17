dd9c98ceed3954e213526096273e340b90a49bbe
Alve Reduan <reddddddddd@Alves-MacBook-Air.local>
2023-08-17 14:22:43 +0600

    Toastify:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,37 @@
+// React imports
 import { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
+
+// URL imports
 import { WSS_URL } from "./constants";
+
+// Toast imports
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   // Websocket
   let ws = null;
   let [updateMessage, setUpdateMessage] = useState("");
 
+  // Toast
+  const notify = (message) =>
+    toast.success(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+
   // Startup
   useEffect(() => {
+    console.log("Startup");
+
     /* Connect to websocket */
     ws = new WebSocket(`${WSS_URL}/update`);
     ws.onopen = () => {
@@ -18,7 +40,9 @@ function App() {
     };
     ws.onmessage = (evt) => {
       // listen to data sent from the websocket server
+      console.log(evt.data);
       setUpdateMessage(evt.data);
+      notify(evt.data);
     };
     ws.onclose = () => {
       console.log("disconnected");
@@ -30,6 +54,18 @@ function App() {
   return (
     <>
       <h1>{updateMessage}</h1>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="dark"
+      />
     </>
   );
 }
